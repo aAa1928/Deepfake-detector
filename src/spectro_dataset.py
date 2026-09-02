62cdddc7b6c37dc02ae832728f43eea0467d1f62
Vatsav Anand <vatsavanand@Vatsavs-MacBook-Pro.local>
2026-09-01 23:45:28 -0400

    added Spectrogram Data Retrieval program

---
+++ src/spectro_dataset.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import torch
+
+class SpectrogramDataset:
+    def __init__(self, AI_path, human_path):
+        self.AI_path = Path('../data/processed/AI/wavs')
+        self.human_path = Path('../data/processed/human/wavs')
+
+        self.file_list = AI_path.glob('*.pt')+human_path.glob('*.pt')
+        self.labels = []
+
+        for file in AI_path.glob('*.pt'):
+            self.labels.append(0.0)
+        for file in human_path.glob('*.pt'):
+            self.labels.append(1.0)
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index, type=None):
+        if type=='AI':
+            file_path = self.AI_path.glob('*.pt')[index]
+            label = 0.0
+        elif type=='human':
+            file_path = self.human_path.glob('*.pt')[index]
+            label = 1.0
+        elif type==None:
+            file_path = self.file_list[index]
+            label = self.labels[index]
+        else:
+            raise ValueError("Invalid type. Must be 'AI', 'human', or None.")
+
+        spectrogram = torch.load(file_path)
+
+        if len(spectrogram.shape) == 2:
+            spectrogram = spectrogram.unsqueeze(0)
+
+        return spectrogram, label
